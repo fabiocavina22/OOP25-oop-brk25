2e520cc868ae1187ac40a477c34744a306a9f160
RichiFrega <riccardofrega05@gmail.com>
2026-06-15 18:50:47 +0200

    risoluzione merge conflict

--- src/main/java/it/unibo/breakout/model/api/Leaderboard.java
+++ src/main/java/it/unibo/breakout/model/api/Leaderboard.java
@@ -6,16 +6,6 @@ package it.unibo.breakout.model.api;
  */
 public interface Leaderboard extends LeaderboardView {
 
-<<<<<<< HEAD
-public interface Leaderboard{
-
-    /**Check if the final score can enter in the leaderboard, if the are fewer than 10 scores in the leaderboard, the newst score always     enter
-    */
-    boolean isHighScore(int result);
-
-    /**Adds the name and the score to the leaderboard, keeps the scores sorted in descending order, and removes the lowest one if there are more than 10 entries
-    */
-=======
     /**
      * Checks if the given score is high enough to enter the leaderboard.
      *
@@ -30,7 +20,6 @@ public interface Leaderboard{
      * @param name the name of the player
      * @param result the score of the player
      */
->>>>>>> cad08518d7447abe16b57fd4ec323c385a395e45
     void add(String name, int result);
 
     /**
